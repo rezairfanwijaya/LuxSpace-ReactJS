492c32d381cdba764ac263c3113c92db3b76c5ce
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-02-20 16:23:31 +0700

    membuat hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+// import header
 import Header from './components/header.js';
+// import hero
+import Hero from './components/hero.js';
 import './tailwind/style.css';
 
 function App() {
   return (
     <>
     <Header/>
+    <Hero/>
     </>
   );
 }
