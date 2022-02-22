1b27b35f7cc8e6af7d4e95f7667a2fcf22873494
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-02-22 13:05:23 +0700

    update video hero

--- src/components/modal.js
+++ src/components/modal.js
@@ -6,13 +6,13 @@ function modal({ handleShowModal }) {
                 <div className="w-screen pb-56 md:w-88 md:pb-56 relative z-50">
                     <div className="absolute w-full h-full">
                         <iframe
-                            title="video"
+                            title="Video"
                             width="100%"
                             height="100%"
-                            src="https://www.youtube.com/watch?v=R9pTTFLxIkc"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write;    encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
+                            src="https://www.youtube.com/embed/3h0_v1cdUIA"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen
                         ></iframe>
                     </div>
                 </div>
